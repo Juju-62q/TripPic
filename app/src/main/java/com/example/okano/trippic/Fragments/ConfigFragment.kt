514cbd28dc7845ddf4856a26d5ef03d971cdefbd
JUJU-surface <fds@fdasf>
2017-10-20 22:56:55 +0900

    change configFragment to PreferenceFragment

--- app/src/main/java/com/example/okano/trippic/Fragments/ConfigFragment.kt
+++ app/src/main/java/com/example/okano/trippic/Fragments/ConfigFragment.kt
@@ -3,6 +3,7 @@ package com.example.okano.trippic.Fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.preference.PreferenceFragmentCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,13 +16,17 @@ import com.example.okano.trippic.R
  * Use the [ConfigFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ConfigFragment : Fragment() {
+class ConfigFragment : PreferenceFragmentCompat() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (arguments != null) {
         }
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
